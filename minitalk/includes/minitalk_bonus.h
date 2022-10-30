059e8146ccd053a4caecc7327934190e3a2a9dbc
김주원 <66457470+juwon357@users.noreply.github.com>
2022-10-30 18:54:09 +0900

    Add files via upload

---
+++ minitalk/includes/minitalk_bonus.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 17:41:43 by juwkim            #+#    #+#             */
+/*   Updated: 2022/10/30 17:41:54 by juwkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <signal.h>
+# include <limits.h>
+# include <stdlib.h>
+
+#endif
